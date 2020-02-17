d4261a740f29cbf7c70c341bece1a7cd6a8be7a7
csamatov96 <csamatov96@gmail.com>
2020-02-16 21:17:59 -0600

    domain added

--- module/route53.tf
+++ module/route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "jenkins_master" {
   zone_id = "${var.zone_id}"
-  name    = "jenkins_master.${var.domain}"
+  name    = "jenkins_master_domain.${var.domain}"
   type    = "A"
   ttl     = "60"
   records = ["${aws_instance.jenkins_master.public_ip}"]
