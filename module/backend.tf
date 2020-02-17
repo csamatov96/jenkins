3d21a098eaddc8155f7fa157a4187e50aef47d24
csamatov96 <csamatov96@gmail.com>
2020-02-16 21:03:18 -0600

    added

---
+++ module/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+backend "s3" {
+bucket = "ato4i-iaac-ireland" #change bucket per environment
+key = "environments/jenkins/us-east-1/tools/tools/jenkins.tfstate"
+region = "eu-west-1" #change region per environment 
+  }
+}
